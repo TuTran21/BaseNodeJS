f06136dd2eff59b3971654490ee2e1d1b3c32c88
TuTran21 <tmtwns@gmail.com>
2019-07-15 14:37:18 +0700

    Assignment 2 - Task 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var _ = require("lodash");
 ////////////////////////////
 /////// ASSIGNMENT 1///////
 ///////////////////////////
-
+console.log("////////////// ASSIGNMENT 1 ////////////////");
 const students = [
   {
     name: "Nam",
@@ -41,3 +41,82 @@ console.log("Female: " + studentsGender.female);
 const studentsName = _.map(students, "name");
 console.log("Students names:");
 console.log(studentsName);
+
+////////////////////////////
+/////// ASSIGNMENT 2///////
+///////////////////////////
+console.log("//////////////// ASSIGNMENT 2 /////////////////");
+const readline = require("readline");
+
+const userInfoInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// userInfoInput.question("What's your name? ", name => {
+//   userInfoInput.question("What's your year of birth? ", yob => {
+//     userInfoInput.question("Where is your hometown? ", hometown => {
+//       user = {
+//         name: name,
+//         yob: yob,
+//         hometown: hometown
+//       };
+//       console.log("Welcome");
+//     });
+//   });
+// });
+
+let user = {
+  name: "",
+  yob: "",
+  hometown: ""
+};
+
+const name = () => {
+  return new Promise((resolve, reject) => {
+    userInfoInput.question("What's your name? ", answer => {
+      user = { ...user, name: answer };
+      resolve();
+    });
+  });
+};
+
+const yob = () => {
+  return new Promise((resolve, reject) => {
+    userInfoInput.question("What's your year of birth? ", answer => {
+      user = { ...user, yob: answer };
+      resolve();
+    });
+  });
+};
+
+const hometown = () => {
+  return new Promise((resolve, reject) => {
+    userInfoInput.question("What's your hometown? ", answer => {
+      user = { ...user, hometown: answer };
+      resolve();
+    });
+  });
+};
+
+const invoke = async () => {
+  await name();
+  await yob();
+  await hometown();
+
+  // DATE CALCULATION
+  const currentYear = new Date().getFullYear();
+  const userAge = currentYear - Number(user.yob);
+  // OUTPUT
+  console.log(
+    "Welcome " +
+      user.name +
+      ", who is now " +
+      userAge +
+      " years old living at " +
+      user.hometown
+  );
+  userInfoInput.close();
+};
+
+invoke();
